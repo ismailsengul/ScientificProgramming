14d6972900164e7b34fae437cce3ab74aca47661
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2024-05-09 03:07:48 +0300

    Updated hw4

--- Assignment4/hw2.jl
+++ Assignment4/hw2.jl
@@ -65,7 +65,7 @@ Feel free to ask questions!
 # ╔═╡ 33e43c7c-f381-11ea-3abc-c942327456b1
 # edit the code below to set your name and kerberos ID (i.e. email without @mit.edu)
 
-student = (name = "Greedies", kerberos_id = "jazz")
+student = (name = "Greedies", kerberos_id = "akdenizcse")
 
 # you might need to wait until all other cells in this notebook have completed running. 
 # scroll around the page to see what's up
